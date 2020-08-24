77dd938cc73e0b0101c8f2f1c15d9be23e7b0ed9
Gina Beth Russell <ginabethrussell@Ginas-MBP.cinci.rr.com>
2020-08-24 19:07:10 -0400

    set up oroject, created branch, completed Task 1

--- index.js
+++ index.js
@@ -7,11 +7,22 @@ const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakf
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
 function createMenuItem(name, cost, category){
-    /* Code here */
+  const newMenuItem = {
+    name: name,
+    price: cost,
+    category: category
+  }
+  return newMenuItem;
 }
 
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
+const taco = createMenuItem("taco", 12, "TexMex");
+const strawberryLemonade = createMenuItem("Strawberry Lemonade", 5, "Drinks");
+const cherryCheesecake = createMenuItem("Cherry Cheesecake", 8, "Desserts");
 
+console.log(taco);
+console.log(strawberryLemonade);
+console.log(cherryCheesecake);
 
 
 /* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter. 
